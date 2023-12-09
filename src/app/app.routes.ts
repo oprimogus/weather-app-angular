b8caa53ee07587cb1ac8f36b22ab19841516d805
oprimogus <gustavo.jesus@proton.me>
2023-12-08 21:02:41 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WheaterHomeComponent } from './modules/pages/wheater-home/wheater-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'wheater',
+        pathMatch: 'full'
+    },
+    {
+        path: 'wheather',
+        component: WheaterHomeComponent
+    }
+];
---
+++ src/app/modules/pages/wheater-home/wheater-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wheater-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './wheater-home.component.html'
+})
+export class WheaterHomeComponent {
+
+}
